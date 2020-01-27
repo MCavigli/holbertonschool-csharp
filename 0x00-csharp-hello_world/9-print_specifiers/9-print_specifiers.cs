682e19979503b0828206b1a94c3e010d37d90238
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-27 14:05:29 -0800

    Used a different way to print the percentage

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-        Console.WriteLine("Percent: {0}%", percent * 100);
+        Console.WriteLine("Percent: {0:0.00%}", percent);
         Console.WriteLine("Currency: ${0}", currency.ToString("#,##0.00"));
 	}
 }
