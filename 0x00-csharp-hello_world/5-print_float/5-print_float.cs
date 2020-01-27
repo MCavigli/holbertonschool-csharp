87b81c93e1e3e08a106227a6359d4cbb21a18c63
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-27 12:48:15 -0800

    Changed Math class to ToString method

--- 0x00-csharp-hello_world/5-print_float/5-print_float.cs
+++ 0x00-csharp-hello_world/5-print_float/5-print_float.cs
@@ -5,6 +5,6 @@ class Program
 	static void Main(string[] args)
         {
 		float number = 3.14159f;
-        Console.WriteLine("Float: {0}", MathF.Round(number * 100f) / 100f);
+        Console.WriteLine("Float: " + MathF.Round(number * 100f) / 100f);
         }
 }
