467a3a31c4d24fcd6839a7e24c148095531c1afe
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-27 13:45:51 -0800

    Initial push for Task 9

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+        Console.WriteLine("Percent: {0}%", percent * 100);
+        Console.WriteLine("Currency: ${0}", currency.ToString("#,##0.00"));
+	}
+}
