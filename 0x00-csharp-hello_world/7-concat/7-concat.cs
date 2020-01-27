897be39afbe89fe54b389ef07ad13ba2996f3719
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-27 14:10:28 -0800

    Added a space when resetting the variable

--- 0x00-csharp-hello_world/7-concat/7-concat.cs
+++ 0x00-csharp-hello_world/7-concat/7-concat.cs
@@ -4,9 +4,9 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		string str1 = "Holberton ";
+		string str1 = "Holberton";
 		string str2 = "School";
-		str1 += str2;
+		str1 += " " + str2;
 		Console.WriteLine("Welcome to {0}!", str1);
 	}
 }
