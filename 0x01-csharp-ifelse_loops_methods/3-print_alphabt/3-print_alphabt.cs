8531c8d67e0ece75327a414898e72a2dc7cb6008
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-28 14:36:55 -0800

    Initial push for Task 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                if (i == 'q' || i == 'e')
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.Write(i);
+                }
+            }
+        }
+    }
+}
