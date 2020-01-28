cc0fea4b602996e14de73382eff84a99b3edaa27
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-28 15:20:21 -0800

    Initial push for Task 8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        if (number < 0)
+        {
+            number *= -1;
+        }
+        Console.Write(number % 10);
+        return (number % 10);
+    }
+}
+
