cd634513db78fee4c8bdf2ec3a0af30b46aaa2ea
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-28 14:48:54 -0800

    Initial push for Task 5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i < 99; i++)
+            {
+                Console.Write("{0}, ", i.ToString("D2"));
+            }
+            Console.WriteLine(i.ToString("D2"));
+        }
+    }
+}
