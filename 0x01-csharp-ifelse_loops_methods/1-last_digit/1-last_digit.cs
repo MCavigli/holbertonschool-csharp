c0852ea0c1d640eb696d96fa4e7d82ab4d5d7f46
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-28 14:25:19 -0800

    Initial push for Task 1

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int lastDigit = number % 10;
+        Console.Write("The last digit of {0} is ", number);
+        if (lastDigit > 5)
+        {
+            Console.WriteLine("{0} and is greater than 5", lastDigit);
+        }
+        else if (lastDigit  == 0)
+        {
+            Console.WriteLine("{0} and is 0", lastDigit);
+        }
+        else
+        {
+            Console.WriteLine("{0} and is less than 6 and not 0", lastDigit);
+        }
+    }
+}
