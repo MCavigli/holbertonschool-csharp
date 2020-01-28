ab0abd0f83257a8b30034e484c36f5364d1733e3
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-28 14:42:10 -0800

    Initial push for Task 4

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 98; i++)
+            {
+                Console.WriteLine("{0} = 0x{1}", i, i.ToString("X"));
+            }
+        }
+    }
+}
