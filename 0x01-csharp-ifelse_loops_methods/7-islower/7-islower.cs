d4a5d606b6ad0e9d644bdb00650fe38237c14a7c
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-28 15:14:33 -0800

    Initial push for Task 7

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// namespace _7_islower
+// {
+    class Character
+    {
+        public static bool IsLower(char c)
+        {
+            if (c >= 'a' && c <= 'z')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+// }
