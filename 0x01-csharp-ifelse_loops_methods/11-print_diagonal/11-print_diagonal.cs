d978f563fc6ba1365e3549b3ed4b03f23167951b
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-28 20:22:56 -0800

    Initial push for Task 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length > 0)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("\\");
+            }
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+    }
+}
