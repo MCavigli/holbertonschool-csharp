fde7b183c1bf72379011263426bee9f2975905fe
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-28 20:50:42 -0800

    Initial push for Task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j;
+            for (i = 0; i <= 8; i++)
+            {
+                for (j = i + 1; j <= 9; j++)
+                {
+                    Console.Write(i);
+                    Console.Write(j);
+                    if (i == 8 && j == 9)
+                    {
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.Write(", ");
+                    }
+                }
+            }
+        }
+    }
+}
