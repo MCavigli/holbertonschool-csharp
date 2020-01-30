d861c7000088a0adfeebb45a0ea7f994cf65b274
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-30 14:21:32 -0800

    Initial push for Task 7

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index < 0 || index >= myList.Count)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+        int searchNum = myList[index];
+        myList.Remove(searchNum);
+        return myList;
+    }
+}
