fb884cefb9fd0069e01e91e71b27a4e0df746681
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-30 14:30:14 -0800

    Initial push for Task 8

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int counter = 0;
+        foreach (KeyValuePair<string, string> kv in myDict)
+            counter++;
+        return counter;
+    }
+}
