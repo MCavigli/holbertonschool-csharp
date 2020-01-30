cc7f5d221fad9045fa003baeb80daefeec45102f
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-30 13:02:49 -0800

    Initial push for Task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int i;
+        int[] newArray = new int[size];
+        for (i = 0; i < newArray.Length; i++)
+        {
+            newArray[i] = i;
+            if (i != newArray.Length - 1)
+            {
+                Console.Write("{0} ", newArray[i]);
+                continue;
+            }
+            Console.WriteLine(newArray[i]);
+        }
+        return newArray;
+    }
+}
