b0e79cf62a031f8b71011543cd8e86e4b7b58108
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-30 13:32:28 -0800

    Initial push for Task 3

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+        int i = array.Length - 1;
+        for (; i >= 0; i--)
+        {
+            if (i == 0)
+            {
+                Console.WriteLine(array[i]);
+                break;
+            }
+            Console.Write("{0} ", array[i]);
+        }
+    }
+}
