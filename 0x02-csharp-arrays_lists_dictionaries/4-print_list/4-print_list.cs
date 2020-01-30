4b04395502914f1d08f5b92695551f4574c0bf35
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-30 13:53:23 -0800

    Initial push for Task 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> newList = new List<int>();
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return newList;
+        }
+        int i;
+        for (i = 0; i < size; i++)
+        {
+            newList.Insert(i, i);
+            if (i == size - 1)
+            {
+                Console.WriteLine(newList[i]);
+                break;
+            }
+            Console.Write("{0} ", newList[i]);
+        }
+        // foreach (int number in newList)
+        // {
+        //     Console.Write("{0} ", number);
+        // }
+        return newList;
+
+    }
+}
