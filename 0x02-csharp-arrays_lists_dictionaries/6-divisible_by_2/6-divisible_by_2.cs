1e84db9a08fac59015bb149b8caf552d59a5bf23
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-30 14:10:28 -0800

    Initial push for Task 6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> newList = new List<bool>();
+        int i = 0;
+        foreach (int number in myList)
+        {
+            if (number % 2 == 0)
+            {
+                newList.Insert(i, true);
+                i++;
+            }
+            else
+            {
+                newList.Insert(i, false);
+                i++;
+            }
+        }
+        return newList;
+    }
+}
