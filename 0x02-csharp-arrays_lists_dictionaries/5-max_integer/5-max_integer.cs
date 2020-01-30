f69ec3391ade3933a3509785bf1beb8805228a4c
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-30 14:03:40 -0800

    Initial push for Task 5

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+        myList.Sort();
+        return myList[myList.Count - 1];
+    }
+}
