bbde66515e4c7ef0d12f185ba7d6f9c1d946a6f0
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-30 18:18:04 -0800

    Initial push for Task 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string winner = "";
+        int bestScore = -1;
+        foreach (KeyValuePair<string, int> pair in myList)
+        {
+            if (pair.Value > bestScore)
+            {
+                bestScore = pair.Value;
+                winner = pair.Key;
+            }
+        }
+        if (bestScore == -1)
+            return "None";
+        return winner;
+    }
+}
