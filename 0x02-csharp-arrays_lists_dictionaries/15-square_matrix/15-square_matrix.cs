1c24fefb6c36f7cc9892f5760e4aeab3bb9ea0b5
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-30 18:46:13 -0800

    Initial push for Task 15

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        // int[,] newMatrix = new int;
+        int[,] newMatrix = myMatrix.Clone() as int[,];
+        for (int i = 0; i < myMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < myMatrix.GetLength(1); j++)
+            {
+                newMatrix[i, j] *= newMatrix[i, j];
+            }
+        }
+        return newMatrix;
+    }
+}
