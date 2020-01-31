36a052ec6c7927a0af9098b10ce251a17318b52d
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-30 18:30:52 -0800

    Initial push for Task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] dArray = new int[5, 5] { 
+                {0, 0, 0, 0, 0}, 
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0}, 
+                {0, 0, 0, 0, 0}
+            };
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (j == 0)
+                    {
+                        Console.Write("{0}", dArray[i, j]);
+                        continue;
+                    }
+                    Console.Write(" {0}", dArray[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
