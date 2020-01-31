a3d68495f87e775b07916e14776e28e83936f969
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-30 19:05:42 -0800

    Initial push for Task 16

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggedArr = new int[3][];
+            jaggedArr[0] = new int[] {0, 1, 2, 3};
+            jaggedArr[1] = new int[] {0, 1, 2, 3, 4, 5, 6};
+            jaggedArr[2] = new int[] {0, 1};
+
+            for (int i = 0; i < jaggedArr.Length; i++)
+            {
+                for (int j = 0; j <jaggedArr[i].Length; j++)
+                {
+                    Console.Write("{0}{1}", jaggedArr[i][j], j == (jaggedArr[i].Length - 1) ? "" : " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
