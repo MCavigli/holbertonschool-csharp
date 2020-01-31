9ded8ecd59f81edbe3385be885514e1793b4fbf4
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-01-30 18:10:46 -0800

    Initial push for Task 12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> keysToSort = new List<string>();
+        foreach (KeyValuePair<string, string> pair in myDict)
+            keysToSort.Add(pair.Key);
+        keysToSort.Sort();
+        foreach (string key in keysToSort)
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+    }
+}
