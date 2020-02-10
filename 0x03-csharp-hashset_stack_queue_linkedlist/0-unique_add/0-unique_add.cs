b7ccdf160591752be46864813d773bf06e5c9cd5
Marc <mcavigli@gmail.com>
2020-02-10 14:54:38 -0800

    Initial push for Task 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+        List<int> newList = myList;
+        newList.Sort();
+        for (int i = 0; i < newList.Count; i++)
+        {
+            if (i == 0 || newList[i] != newList[i - 1])
+                sum += myList[i];
+        }
+        return (sum);
+    }
+}
