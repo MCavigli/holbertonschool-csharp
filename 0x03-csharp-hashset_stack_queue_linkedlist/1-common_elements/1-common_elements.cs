97716e283b9c590ee5e6e078bbd3cfb5be7912a7
Marc <mcavigli@gmail.com>
2020-02-10 15:40:18 -0800

    Initial push for Task 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> newList = new List<int>();
+        foreach (int digit1 in list1)
+        {
+            foreach (int digit2 in list2)
+            {
+                if (digit1 == digit2)
+                {
+                    newList.Add(digit1);
+                }
+            }
+        }
+        return (newList);
+    }
+}
