ec86ddc2c7363a034c69d0ee8a7e83d2fb253881
Marc <mcavigli@gmail.com>
2020-02-10 20:21:31 -0800

    Added edge cases

--- 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -5,10 +5,31 @@ class LList
 {
     public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
     {
+        if (myLList == null)
+            return (null);
         LinkedListNode<int> current = myLList.First;
+        if (current.Next == null)
+        {
+            if (current.Value > n)
+            {
+                myLList.AddFirst(n);
+                return (myLList.First);
+            }
+            else
+            {
+                myLList.AddLast(n);
+                return (myLList.Last);
+            }
+        }
         while (current != null)
         {
-            if (n < current.Next.Value)
+
+            if (n > current.Value && current.Next == null)
+            {
+                myLList.AddLast(n);
+                return (current);
+            }
+            if (n < current.Next.Value && n > current.Value)
             {
                 myLList.AddAfter(current, n);
                 return (current);
