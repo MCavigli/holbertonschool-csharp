1d0df1d0f9bf5005fbc4f00d7adbd814e23b11b4
Marc <mcavigli@gmail.com>
2020-02-10 20:09:35 -0800

    Initial push for Task 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current = myLList.First;
+        while (current != null)
+        {
+            if (n < current.Next.Value)
+            {
+                myLList.AddAfter(current, n);
+                return (current);
+            }
+            current = current.Next;
+        }
+        return (current);
+    }
+}
