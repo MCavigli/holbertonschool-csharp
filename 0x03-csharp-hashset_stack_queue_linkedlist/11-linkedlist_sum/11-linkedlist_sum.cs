5225d1091eafb46022852382810b93d7cc4f14c8
Marc <mcavigli@gmail.com>
2020-02-10 19:38:53 -0800

    Initial push for Task 11

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int total = 0;
+        foreach (int value in myLList)
+            total += value;
+        return (total);
+    }
+}
