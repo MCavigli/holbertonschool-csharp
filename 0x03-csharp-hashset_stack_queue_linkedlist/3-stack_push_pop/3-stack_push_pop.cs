74bdc025684ec2c2923221f91f52dc3d185383c3
Marc <mcavigli@gmail.com>
2020-02-10 16:41:47 -0800

    Initial push for Task 3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        bool present = aStack.Contains(search);
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+        if (aStack.Count == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine("Top Item: {0}", aStack.Peek());
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, present);
+        if (present)
+        {
+            while (aStack.Pop() != search)
+            {
+                continue;
+            }
+        }
+        aStack.Push(newItem);
+        return (aStack);
+    }
+}
