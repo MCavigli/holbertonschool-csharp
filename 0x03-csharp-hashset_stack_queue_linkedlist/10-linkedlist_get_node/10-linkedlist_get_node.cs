499be5ee4092442f0300b1bbc35eceaee8f86341
Marc <mcavigli@gmail.com>
2020-02-10 19:34:07 -0800

    Initial push for Task 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (n > myLList.Count)
+            return (0);
+        int counter = 0;
+        foreach (int node in myLList)
+        {
+            if (counter == n)
+                return (node);
+            counter += 1;
+        }
+        return (0);
+    }
+}
