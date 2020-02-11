669eb8f9f823062c01aa9ad44bded41257712fbb
Marc <mcavigli@gmail.com>
2020-02-10 19:13:06 -0800

    Initial push for Task 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int counter = 0;
+        foreach (int node in myLList)
+        {
+            if (value == node)
+                return (counter);
+            counter += 1;
+        }
+        return (-1);
+    }
+}
