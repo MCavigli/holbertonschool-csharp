eb02343ff8ad30a01e4d6b914fd0c128a11145ea
Marc <mcavigli@gmail.com>
2020-02-10 22:06:07 -0800

    Changed how i is initialized

--- 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -5,8 +5,9 @@ class LList
 {
     public static void Delete(LinkedList<int> myLList, int index)
     {
+        int i = 0;
         LinkedListNode<int> current = myLList.First;
-        for (int i = 0; i < index && current.Next != null; i++)
+        for (; i < index && current.Next != null; i++)
             current = current.Next;
         if (i == index)
             myLList.Remove(current);
