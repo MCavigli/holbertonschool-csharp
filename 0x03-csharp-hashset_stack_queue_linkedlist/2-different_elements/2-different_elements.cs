ce461c51e3ee0a5dd2333949479705428b224fbb
Marc <mcavigli@gmail.com>
2020-02-10 16:09:37 -0800

    Initial push for Task 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> newList = new List<int>();
+
+        foreach (int digit in list1)
+        {
+            if (list2.Contains(digit))
+                continue;
+            else
+                newList.Add(digit);
+        }
+        foreach (int digit in list2)
+        {
+            if (list1.Contains(digit))
+                continue;
+            else
+                newList.Add(digit);
+        }
+        newList.Sort();
+
+
+        return (newList);
+    }
+}
