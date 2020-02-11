91eb0590453fa15f3af1882ca8246e53a8620b95
Marc <mcavigli@gmail.com>
2020-02-10 16:56:55 -0800

    Initial push for Task 4

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        bool present = aQueue.Contains(search);
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        if (aQueue.Count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        aQueue.Enqueue(newItem);
+        Console.WriteLine("Queue contains: \"{0}\": {1}", search, present);
+        if (present == true)
+        {
+            while (aQueue.Dequeue() != search)
+            { }
+        }
+        return (aQueue);
+    }
+}
