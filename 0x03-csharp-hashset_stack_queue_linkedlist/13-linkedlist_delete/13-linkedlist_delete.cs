9da95f591d23adef0fd55845f4f6af361fcaf1e4
Marc <mcavigli@gmail.com>
2020-02-10 21:20:43 -0800

    Initial push for Task 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> current = myLList.First;
+        for (int i = 0; i < index; i++)
+            current = current.Next;
+        myLList.Remove(current);
+
+    }
+}
