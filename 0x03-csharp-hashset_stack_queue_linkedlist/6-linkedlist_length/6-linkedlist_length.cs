19dd9f44b9bdd687435cc377075932a7a73d4cc2
Marc <mcavigli@gmail.com>
2020-02-10 19:02:42 -0800

    Initial push for Task 6

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int counter = 0;
+        foreach (int node in myLList)
+            counter += 1;
+        return (counter);
+    }
+}
