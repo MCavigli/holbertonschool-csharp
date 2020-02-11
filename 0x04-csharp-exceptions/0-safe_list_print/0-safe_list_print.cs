26987d261ce40991f3de988e653c08583ebffa22
Marc <mcavigli@gmail.com>
2020-02-11 00:44:23 -0800

    Initial push for Task 0

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int i = 0;
+        try
+        {
+            for (; i < n; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+        }
+        catch (ArgumentOutOfRangeException)
+        { }
+        return (i);
+    }
+}
