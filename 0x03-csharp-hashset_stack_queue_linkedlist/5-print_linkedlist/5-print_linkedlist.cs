78b79001a67c8e27e1c33449c0f7bcc8e7292274
Marc <mcavigli@gmail.com>
2020-02-10 18:58:07 -0800

    Initial push for Task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> newList = new LinkedList<int>();
+        if (size < 0)
+            return (newList);
+        for (int i = 0; i < size; i++)
+            newList.AddLast(i);
+        foreach (int node in newList)
+            Console.WriteLine(node);
+        return (newList);
+    }
+}
