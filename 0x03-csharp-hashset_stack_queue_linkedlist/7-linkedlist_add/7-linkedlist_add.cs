7333c4f05cfeb91c6de723f221ef91d8eb88255d
Marc <mcavigli@gmail.com>
2020-02-10 19:07:35 -0800

    Initial push for Task 7

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        return (myLList.AddFirst(n));
+    }
+}
