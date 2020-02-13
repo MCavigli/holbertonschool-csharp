fb4b5e7ce8a0f2bb07c28ecb671f8c26c3c81e23
Marc <mcavigli@gmail.com>
2020-02-13 13:27:26 -0800

    Changed paramenter name from health to value

--- 0x06-csharp-classes/3-enemy/3-enemy.cs
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -30,14 +30,14 @@ namespace Enemies
 		/// <summary>
 		/// Constructor for zombie class with health field.
 		/// </summary>
-		/// <param name="health">Represents a zombie instance's health.</param>
-		public Zombie(int health)
+		/// <param name="value">Represents a zombie instance's health.</param>
+		public Zombie(int value)
 		{
-			if (health < 0)
+			if (value < 0)
 			{
 				throw new ArgumentException("Health must be greater than or equal to 0");
 			}
-			this.health = health;
+			health = health;
 		}
 	}
 }
