83b7b32a918282fb41624b57d4fd4d29c7d05fab
Marc <mcavigli@gmail.com>
2020-02-13 13:58:04 -0800

    Initial pus for Task 5

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Enemies
+{
+	/// <summary>
+	/// Provides a template for a zombie enemy.
+	/// </summary>
+	public class Zombie
+	{
+		/// <summary>
+		/// Represents a zombie instance's health.
+		/// </summary>
+		private int health;
+
+		/// <summary>
+		/// Represents the name of the zombie.
+		/// </summary>
+		private string name = "(No name)";
+
+		/// <summary>
+		/// Getter and setter for zombie's name.
+		/// </summary>
+		/// <value>Gets and sets the value of name.</value>
+		public string Name
+		{
+			get { return (name); }
+			set
+			{ name = value; }
+		}
+
+		/// <summary>
+		/// Method that retrieves the health of a zombie instance.
+		/// </summary>
+		/// <returns>The zombie isntance's health.</returns>
+		public int GetHealth()
+		{
+			return (this.health);
+		}
+
+		/// <summary>
+		/// Defines a custom toString() method.
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			return ("Zombie name: " + name + " / Health: " + health);
+		}
+
+		/// <summary>
+		/// Basic constructor for zombie class.
+		/// </summary>
+		public Zombie()
+		{ }
+
+		/// <summary>
+		/// Constructor for zombie class with health field.
+		/// </summary>
+		/// <param name="value">Represents a zombie instance's health.</param>
+		public Zombie(int value)
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Health must be greater than or equal to 0");
+			}
+			health = value;
+		}
+
+
+	}
+}
