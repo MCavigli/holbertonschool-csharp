8a6581463eb2b6c931091dd2ac693e7edda5eefc
Marc <mcavigli@gmail.com>
2020-02-13 13:11:58 -0800

    Initial push for Task 3

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Enemies
+{
+	/// <summary>
+	/// Provides a template for a zombie enemy.
+	/// </summary>
+	public class Zombie
+	{
+		/// <summary>
+		/// Represents a zombie instance's health.
+		/// </summary>
+		private int health;
+
+		/// <summary>
+		/// Method that retrieves the health of a zombie instance.
+		/// </summary>
+		/// <returns>The zombie isntance's health.</returns>
+		public int GetHealth()
+		{
+			return (this.health);
+		}
+
+		/// <summary>
+		/// Basic constructor for zombie class.
+		/// </summary>
+		public Zombie()
+		{ }
+
+		/// <summary>
+		/// Constructor for zombie class with health field.
+		/// </summary>
+		/// <param name="health">Represents a zombie instance's health.</param>
+		public Zombie(int health)
+		{
+			if (health < 0)
+			{
+				throw new ArgumentException("Health must be greater than or equal to 0");
+			}
+			this.health = health;
+		}
+	}
+}
