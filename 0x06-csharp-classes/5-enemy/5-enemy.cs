0871fd4b7aa9c1bbe27a8182045fdea1d12db64e
Marc <mcavigli@gmail.com>
2020-02-13 14:08:31 -0800

    Remvoed parenthesis around return statement

--- 0x06-csharp-classes/5-enemy/5-enemy.cs
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -43,7 +43,7 @@ namespace Enemies
 		/// <returns></returns>
 		public override string ToString()
 		{
-			return ("Zombie name: " + name + " / Total Health: " + health);
+			return "Zombie name: " + name + " / Total Health: " + health;
 		}
 
 		/// <summary>
@@ -64,7 +64,5 @@ namespace Enemies
 			}
 			health = value;
 		}
-
-
 	}
 }
