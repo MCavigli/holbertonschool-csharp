bcca3e207a65aab82866f7fe23469b61337dee4d
Marc <mcavigli@gmail.com>
2020-02-13 12:51:00 -0800

    Added XML documentation

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -2,10 +2,19 @@
 
 namespace Enemies
 {
+	/// <summary>
+	/// Provides a template for a zombie enemy.
+	/// </summary>
 	public class Zombie
 	{
+		/// <summary>
+		/// Represents a zombie instance's health.
+		/// </summary>
 		public int health;
 
+		/// <summary>
+		/// Constructor for zombie class.
+		/// </summary>
 		public Zombie()
 		{ }
 	}
