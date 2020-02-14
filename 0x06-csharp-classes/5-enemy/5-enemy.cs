85f368e893abf47de8453e633eccd1b953cab19c
Marc <mcavigli@gmail.com>
2020-02-13 20:02:21 -0800

    Added this to toString() return expression

--- 0x06-csharp-classes/5-enemy/5-enemy.cs
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -43,7 +43,7 @@ namespace Enemies
 		/// <returns></returns>
 		public override string ToString()
 		{
-			return "Zombie name: " + name + " / Total Health: " + health;
+			return "Zombie name: " + this.name + " / Total Health: " + this.health;
 		}
 
 		/// <summary>
