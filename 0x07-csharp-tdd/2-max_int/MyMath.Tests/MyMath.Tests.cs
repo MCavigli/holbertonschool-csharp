f3fc7aee623af16557277adcacfc6c68b0ea92a9
Marc <mcavigli@gmail.com>
2020-02-18 15:19:54 -0800

    Initial push for Task 2

---
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using MyMath;
+using System.Collections.Generic;
+
+namespace MyMath.Tests
+{
+	public class Operations_Tests
+	{
+		private List<int> myList1;
+
+		[SetUp]
+		public void Setup()
+		{
+			myList1 = new List<int>() { 98, -10, 0, 32, 972, 12, 1024, -727, -98, 50, 8 };
+		}
+
+		[Test]
+		public void Max_SuccessfulExample()
+		{
+			int result = Operations.Max(myList1);
+			Assert.AreEqual(1024, result);
+		}
+		[Test]
+		public void Max_Nums_IsZero()
+		{
+			List<int> myListEmpty = new List<int>();
+			int result;
+			result = Operations.Max(myListEmpty);
+			Assert.AreEqual(0, result);
+		}
+	}
+}
---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MyMath
+{
+	/// <summary>
+	/// Holds method to return max number in a list.
+	/// </summary>
+	public class Operations
+	{
+		/// <summary>
+		/// Method returns the largest integer in a list.
+		/// </summary>
+		/// <param name="nums">List of integers.</param>
+		/// <returns></returns>
+		public static int Max(List<int> nums)
+		{
+			if (nums.Count == 0)
+				return (0);
+			nums.Sort();
+			return (nums[nums.Count - 1]);
+		}
+	}
+}
