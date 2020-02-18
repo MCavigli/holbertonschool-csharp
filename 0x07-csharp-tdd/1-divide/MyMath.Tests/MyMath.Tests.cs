873b6160308b8c7e354bfbef78a4ef4b02eac50f
Marc <mcavigli@gmail.com>
2020-02-18 14:51:05 -0800

    Initial push for Task 1

---
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,85 @@
+using NUnit.Framework;
+using MyMath;
+using System;
+using System.Collections.Generic;
+
+namespace MyMath.Tests
+{
+	[TestFixture]
+	public class Matrix_Tests
+	{
+		private int[,] array2D;
+
+		[SetUp]
+		public void SetUp()
+		{
+			array2D = new int[,] {
+				{ 1, 2 },
+				{ 3, 4 },
+				{ 5, 6 },
+				{ 7, 8 }
+				};
+		}
+		[TearDown]
+		public void CleanUp()
+		{
+			array2D = null;
+		}
+
+		[Test]
+		public void Divide_SuccessfulExample()
+		{
+			int[,] finishedArray = new int[,] {
+				{ 0, 1 },
+				{ 1, 2 },
+				{ 2, 3 },
+				{ 3, 4 },
+			};
+			int num = 2;
+			int[,] dividedMatrix = Matrix.Divide(array2D, num);
+			Assert.AreEqual(finishedArray, dividedMatrix);
+		}
+
+		[Test]
+		public void Divide_Number_IsZero()
+		{
+			int num = 0;
+			int[,] dividedMatrix;
+			try
+			{
+				dividedMatrix = Matrix.Divide(array2D, num);
+			}
+			catch (DivideByZeroException)
+			{
+				dividedMatrix = Matrix.Divide(array2D, num);
+				Assert.IsNull(dividedMatrix);
+			}
+		}
+		// [Test]
+		// public void Divide_Number_IsDifferentType()
+		// {
+		// 	double num = 3.3;
+		// 	int[,] dividedMatrix;
+		// 	try
+		// 	{
+		// 		dividedMatrix = Matrix.Divide(array2D, num);
+		// 	}
+		// 	catch (ArgumentException)
+		// 	{
+		// 		Assert.Fail();
+		// 	}
+		// }
+		[Test]
+		public void Divide_Matrix_IsMatrix()
+		{
+			Assert.AreEqual(array2D.GetType(), typeof(int[,]));
+		}
+		[Test]
+		public void Divide_Matrix_IsNotMatrix()
+		{
+			int[] falseArray = { 1, 2, 3, 4 };
+			Assert.AreNotEqual(falseArray.GetType(), typeof(int));
+		}
+
+	}
+}
---
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyMath
+{
+	/// <summary>
+	/// Holds methods to alter a matrix.
+	/// </summary>
+	public class Matrix
+	{
+		/// <summary>
+		/// Divides each element of a matrix.
+		/// </summary>
+		/// <param name="matrix">The matrix to be divided.</param>
+		/// <param name="num">The number to divide all matrix elements by.</param>
+		/// <returns></returns>
+		public static int[,] Divide(int[,] matrix, int num)
+		{
+			int[,] newMatrix = matrix.Clone() as int[,];
+			if (num == 0)
+			{
+				Console.WriteLine("Num cannot be 0");
+				return (null);
+			}
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					newMatrix[i, j] /= num;
+				}
+			}
+			return (newMatrix);
+		}
+	}
+}
