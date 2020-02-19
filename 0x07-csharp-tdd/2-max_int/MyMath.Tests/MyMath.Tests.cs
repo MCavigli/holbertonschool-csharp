b076e6c6a7568a6c9e7859744901ee8aa75faf1b
Marc <mcavigli@gmail.com>
2020-02-19 15:24:36 -0800

    Added tests

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using MyMath;
 using System.Collections.Generic;
+using System;
 
 namespace MyMath.Tests
 {
@@ -29,6 +30,36 @@ namespace MyMath.Tests
 			int result;
 			result = Operations.Max(myListEmpty);
 			Assert.AreEqual(0, result);
+		}
+		[Test]
+		public void Max_MaxNum_MaxNumAtEnd()
+		{
+			List<int> endList = new List<int>() { 98, -10, 0, 32, 972, 12, 1024, -727, -98, 50, 8, 5000 };
+			int result;
+			result = Operations.Max(endList);
+			Assert.AreEqual(5000, result);
+		}
+		[Test]
+		public void Max_MaxNum_MaxNumAtBeginning()
+		{
+			List<int> begList = new List<int>() { 5000, 98, -10, 0, 32, 972, 12, 1024, -727, -98, 50, 8 };
+			int result;
+			result = Operations.Max(begList);
+			Assert.AreEqual(5000, result);
+		}
+		[Test]
+		public void Max_EmptyList()
+		{
+			List<int> eList = new List<int>() {};
+			int result;
+			try
+			{
+				result = Operations.Max(eList);
+			}
+			catch (NullReferenceException)
+			{
+				Assert.IsNull(eList);
+			}
 		}
 	}
 }
