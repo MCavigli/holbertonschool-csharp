bd96ec1f914bb7c7c8787c9d38d9e2f03f71619b
Marc <mcavigli@gmail.com>
2020-02-19 00:36:35 -0800

    Added tests

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -24,5 +24,38 @@ namespace MyMath.Tests
 			int result = Operations.Add(a, b);
 			Assert.That(result, Is.InstanceOf(typeof(int)));
 		}
+
+		[Test]
+		public void Add_IsInteger_NegativeInteger()
+		{
+			int a = -5;
+			int b = 4;
+			int result = Operations.Add(a, b);
+			Assert.AreEqual(result, -1);
+		}
+		[Test]
+		public void Add_IsInteger_NegativeIntegers()
+		{
+			int a = -5;
+			int b = -4;
+			int result = Operations.Add(a, b);
+			Assert.AreEqual(result, -9);
+		}
+		[Test]
+		public void Add_IsInteger_AddZero()
+		{
+			int a = 0;
+			int b = 4;
+			int result = Operations.Add(a, b);
+			Assert.AreEqual(result, 4);
+		}
+		[Test]
+		public void Add_IsInteger_AllZeroes()
+		{
+			int a = 0;
+			int b = 0;
+			int result = Operations.Add(a, b);
+			Assert.AreEqual(result, 0);
+		}
 	}
 }
