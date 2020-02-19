bfb407b363be3a628f5dbf3c6c07454009bfe4ce
Marc <mcavigli@gmail.com>
2020-02-18 21:06:10 -0800

    Initial push for Task 4

---
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using Text;
+
+namespace Text.Tests
+{
+	public class Str_Tests
+	{
+		private string s;
+		private string s2;
+		private string s3;
+		[SetUp]
+		public void SetUp()
+		{
+			s = "heywhatsuphowsitgoing";
+			s2 = "norepats";
+			s3 = "allrepeatsrpts";
+		}
+		[Test]
+		public void Str_NoRepeats()
+		{
+			int result = Str.UniqueChar(s2);
+			Assert.AreEqual(0, result);
+		}
+		[Test]
+		public void Str_Repeats()
+		{
+			int result = Str.UniqueChar(s);
+			Assert.AreEqual(1, result);
+		}
+		[Test]
+		public void Str_AllRepeats()
+		{
+			int result = Str.UniqueChar(s3);
+			Assert.AreEqual(-1, result);
+		}
+	}
+}
---
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Text
+{
+	/// <summary>
+	/// Class holds method to finds the index of the first non-repeating character of a stringj.
+	/// </summary>
+	public class Str
+	{
+		/// <summary>
+		/// Returns index of first non-repeating character of a string.
+		/// </summary>
+		/// <param name="s">String to check.</param>
+		/// <returns></returns>
+		public static int UniqueChar(string s)
+		{
+			int i = 0;
+			int j;
+			List<char> repeat = new List<char>();
+			List<int> noRepeat = new List<int>();
+			for (; i <= s.Length - 1; i++)
+			{
+
+				for (j = i + 1; j <= s.Length - 1; j++)
+				{
+
+					if (repeat.Contains(s[i]))
+						break;
+					else if (s[i] == s[j])
+					{
+						repeat.Add(s[i]);
+						break;
+					}
+					if (j == s.Length - 1 && !repeat.Contains(s[i]))
+						noRepeat.Add(i);
+				}
+			}
+			if (noRepeat.Count > 0)
+				return (noRepeat[0]);
+			return (-1);
+
+
+		}
+	}
+}
