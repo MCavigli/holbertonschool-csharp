44cf735c29e1c9ff9d9b985d1d65c65aa77288b5
Marc <mcavigli@gmail.com>
2020-02-20 12:28:07 -0800

    Initial push for Task 1

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Class holding method to check if an object is an integer.
+/// </summary>
+class Obj
+{
+	/// <summary>
+	/// Returns true if object is an integer, false if not.
+	/// </summary>
+	/// <param name="obj">The object to check.</param>
+	/// <returns>True if object is an integer, false if not.</returns>
+	public static bool IsOfTypeInt(object obj)
+	{
+		if (obj is int)
+			return (true);
+		else
+			return (false);
+	}
+}
