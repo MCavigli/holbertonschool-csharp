7afe5dcd16e0dc7cf05c8b8d28a2f72228c4516b
Marc <mcavigli@gmail.com>
2020-02-20 12:42:59 -0800

    Initial push for Task 1

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Class holding methods to work on an object of unknown type.
+/// </summary>
+class Obj
+{
+	/// <summary>
+	/// Method that checks if an object is an instance of an Array or an instance of a class that inherits from an Array.
+	/// </summary>
+	/// <param name="obj">The object to check.</param>
+	/// <returns>True if object is an instance of an Array, false if not.</returns>
+	public static bool IsInstanceOfArray(object obj)
+	{
+		if (obj is Array)
+			return true;
+		else
+			return false;
+	}
+}
