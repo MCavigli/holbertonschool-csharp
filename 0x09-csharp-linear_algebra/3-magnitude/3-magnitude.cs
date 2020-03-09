233c4e0a84240ec92da343cc1ad9c2b27f503cc9
Marc <mcavigli@gmail.com>
2020-03-09 16:38:27 -0700

    Initial push for tasks 0-3

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class VectorMath
+{
+	/// <summary>
+	/// Finds the length of a 2D or 3D vector.
+	/// </summary>
+	/// <param name="vector">The vector to find the length of.</param>
+	/// <returns>The length of the vector or -1 if the vector isn't 2D or 3D.</returns>
+	public static double Magnitude(double[] vector)
+	{
+		int i;
+		double result = 0.0;
+		if (vector.Length < 2 || vector.Length > 3)
+			return (-1);
+
+		for (i = 0; i < vector.Length; i++)
+			vector[i] = Math.Round(vector[i] * vector[i], 2);
+
+		foreach (double dub in vector)
+			result += result + dub;
+		return (result);
+	}
+}
