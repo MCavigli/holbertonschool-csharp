4b47b219b0646eaee20991b7b4d3a2c4025113b2
Marc <mcavigli@gmail.com>
2020-03-10 13:46:48 -0700

    Initial push for tasks 4-9

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class VectorMath
+{
+	/// <summary>
+	/// Function that finds the length of a 2D or 3D vector.
+	/// </summary>
+	/// <param name="vector1">The first vector.</param>
+	/// <param name="vector2">The second vector.</param>
+	/// <returns>The length of the vector or -1 if either vector is not a 2D or 3D vector.</returns>
+	public static double[] Add(double[] vector1, double[] vector2)
+	{
+		int i;
+
+		if (vector1.Length != vector2.Length)
+			return (-1);
+		if (vector1.Length < 2 || vector1.Length > 3 || vector2.Length < 2 || vector2.Length > 3)
+			return (-1);
+
+		double vector3 = new double(vector1.Length);
+
+		for (i = 0; i < vector1.Length; i++)
+			vector3[i] = vector1[i] + vector2[i];
+
+		return (vector3);
+	}
+}
---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class VectorMath
+{
+	public static double[] Multiply(double[] vector, double scalar)
+	{
+		int i;
+
+		if (vector.Length < 2 || vector.Length > 3)
+			return (-1);
+
+		for (i = 0; i < vector.Length; i++)
+			vector[i] = Math.Round(vector[i] * vector[i], 2);
+		return (vector);
+	}
+}
