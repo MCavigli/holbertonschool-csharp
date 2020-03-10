de20cf0ed6cc858bb18b08d9deb1a374b51885b1
Marc <mcavigli@gmail.com>
2020-03-10 14:08:38 -0700

    Put fail return into vector

--- 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -5,9 +5,10 @@ class VectorMath
 	public static double[] Multiply(double[] vector, double scalar)
 	{
 		int i;
+		double[] failVector = new double[] { -1 };
 
 		if (vector.Length < 2 || vector.Length > 3)
-			return (-1);
+			return (failVector);
 
 		for (i = 0; i < vector.Length; i++)
 			vector[i] = Math.Round(vector[i] * vector[i], 2);
