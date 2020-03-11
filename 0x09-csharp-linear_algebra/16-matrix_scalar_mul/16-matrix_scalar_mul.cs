add94b904ccc408e3992bdd92d9d8891ff1c6b7e
Marc <mcavigli@gmail.com>
2020-03-11 12:35:53 -0700

    Initial push for Task 16

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class MatrixMath
+{
+	/// <summary>
+	/// Multiplies a matrix by a scalar.
+	/// </summary>
+	/// <param name="matrix">The matrix.</param>
+	/// <param name="scalar">The scalar.</param>
+	/// <returns>A new matrix.</returns>
+	public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+	{
+		double[,] newMatrix;
+		double[,] failMatrix = { { -1 } };
+		int h = matrix.GetLength(0);
+		int w = matrix.GetLength(1);
+		int i, j;
+
+		if (h == 2 && w == 2)
+			newMatrix = new double[2, 2];
+		else if (h == 3 && w == 3)
+			newMatrix = new double[3, 3];
+		else
+			return (failMatrix);
+		for (i = 0; i < h; i++)
+		{
+			for (j = 0; j < w; j++)
+			{
+				newMatrix[i, j] = matrix[i, j] * scalar;
+			}
+		}
+		return (newMatrix);
+	}
+}
