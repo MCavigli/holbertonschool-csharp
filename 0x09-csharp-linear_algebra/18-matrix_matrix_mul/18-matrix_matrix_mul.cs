2eccd478dde6fda349f71a7df43a0e986cf78057
Marc <mcavigli@gmail.com>
2020-03-11 13:17:15 -0700

    Initial push for Task 18

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class MatrixMath
+{
+	// public void Main()
+	// {
+	// 	double[,] m1 = { { 2, 3 }, { -1, 0 } };
+	// 	double[,] m2 = { { 1, 7 }, { -8, -2 } };
+	// 	int h = Multiply(m1, m2).GetLength(0);
+	// 	int w = Multiply(m1, m2).GetLength(1);
+	// 	double[,] nm = Multiply(m1, m2);
+	// 	int i, j;
+	// 	for (i = 0; i < h; i++)
+	// 	{
+	// 		for (j = 0; j < w; j++)
+	// 		{
+	// 			Console.Write("{0} ", nm[i, j]);
+	// 		}
+	// 		Console.WriteLine();
+	// 	}
+	// }
+	public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+	{
+		int h1 = matrix1.GetLength(0);
+		int w1 = matrix1.GetLength(1);
+		int h2 = matrix2.GetLength(0);
+		int w2 = matrix2.GetLength(1);
+		double[,] failMatrix = { { -1 } };
+		double[,] newMatrix;
+		int i, j, k;
+		// int result = 0;
+		int prod = 0;
+
+		if (w1 == h2)
+			newMatrix = new double[h1, w2];
+		else
+			return (failMatrix);
+
+		for (i = 0; i < h1; i++)
+		{
+			// result = 0;
+			for (k = 0; k < w1; k++)
+			{
+				for (j = 0; j < w2; j++)
+				{
+					prod += matrix1[i, k] * matrix2[k, j];
+				}
+				newMatrix[i, k] = prod;
+				// result += prod;
+			}
+		}
+	}
+}
