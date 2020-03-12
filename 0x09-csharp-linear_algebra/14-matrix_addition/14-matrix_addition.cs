34c8abe0d1cb130c0c748c11c8603d2bc4fae77b
Marc <mcavigli@gmail.com>
2020-03-12 15:23:38 -0700

    Added conditionals

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -15,10 +15,10 @@ class MatrixMath
 		int i, j;
 		double[,] newMatrix;
 
-		if (matrix1.GetLength(0) == 2 && matrix2.GetLength(1) == 2 &&
+		if (matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2 &&
 			matrix1.GetLength(0) == matrix2.GetLength(0) && matrix1.GetLength(1) == matrix2.GetLength(1))
 			newMatrix = new double[2, 2];
-		else if (matrix1.GetLength(0) == 3 && matrix2.GetLength(1) == 3 &&
+		else if (matrix1.GetLength(0) == 3 && matrix1.GetLength(1) == 3 &&
 			matrix1.GetLength(0) == matrix2.GetLength(0) && matrix1.GetLength(1) == matrix2.GetLength(1))
 			newMatrix = new double[3, 3];
 		else
