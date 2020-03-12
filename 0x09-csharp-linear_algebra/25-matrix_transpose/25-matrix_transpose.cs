f9197fa2a4ac289dc37af0141d1b51d37ce7f5b2
Marc <mcavigli@gmail.com>
2020-03-11 20:57:31 -0700

    Initial push for Task 23

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class MatrixMath
+{
+	/// <summary>
+	/// Transposes a matrix.
+	/// </summary>
+	/// <param name="matrix">The matrix.</param>
+	/// <returns>The new, transposed matrix.</returns>
+	public static double[,] Transpose(double[,] matrix)
+	{
+		double[,] failMatrix = { { -1 } };
+		double[,] newMatrix;
+
+		if (matrix.Length == 0)
+			return failMatrix;
+
+		for (int i = 0; i < matrix.GetLength(0); i++)
+		{
+			for (int j = 0; j < matrix.GetLength(1); j++)
+			{
+				newMatrix[i, j] = matrix[j, i];
+			}
+		}
+		return (newMatrix);
+	}
+}
