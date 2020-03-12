83e6b1ee11dca6ce6094fe950b440e3570497c7e
Marc <mcavigli@gmail.com>
2020-03-11 18:35:54 -0700

    Simplified conditional to return a failed matrix

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -11,14 +11,12 @@ class MatrixMath
 	public static double[,] Rotate2D(double[,] matrix, double angle)
 	{
 		double[,] failMatrix = { { -1 } };
-		double[,] newMatrix;
+		double[,] newMatrix = new double[2, 2];
 		double[,] rotateMatrix = { { Math.Cos(angle), Math.Sin(angle) }, { Math.Sin(angle) * -1, Math.Cos(angle) } };
 		int i, j, k;
 		double prod = 0;
 
-		if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
-			newMatrix = new double[2, 2];
-		else
+		if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
 			return (failMatrix);
 
 		for (i = 0; i < 2; i++)
