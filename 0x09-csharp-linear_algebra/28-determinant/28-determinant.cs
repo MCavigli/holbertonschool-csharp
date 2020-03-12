6a7916bca4125996af87cbb3d6132527b695d01c
Marc <mcavigli@gmail.com>
2020-03-11 21:30:05 -0700

    Added Math.Round() to 3D matrix

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -17,9 +17,10 @@ class MatrixMath
 		}
 		else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
 		{
-			d = matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] - matrix[2, 1] * matrix[1, 2]) -
+			d = Math.Round(
+				matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] - matrix[2, 1] * matrix[1, 2]) -
 				matrix[0, 1] * (matrix[0, 1] * matrix[2, 2] - matrix[2, 1] * matrix[0, 2]) +
-				matrix[0, 2] * (matrix[0, 1] * matrix[1, 2] - matrix[1, 1] * matrix[0, 2]);
+				matrix[0, 2] * (matrix[0, 1] * matrix[1, 2] - matrix[1, 1] * matrix[0, 2]), 2);
 		}
 		else
 		{
