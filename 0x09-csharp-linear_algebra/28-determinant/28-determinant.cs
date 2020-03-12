f70aaddccead48addf2b2a5817d142ded50793aa
Marc <mcavigli@gmail.com>
2020-03-11 21:27:00 -0700

    Changed determinant variable

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -9,7 +9,7 @@ class MatrixMath
 	/// <returns>The determinant of the matrix, -1 if not a 2D or 3D matrix.</returns>
 	public static double Determinant(double[,] matrix)
 	{
-		double d = -1;
+		double d = 0;
 
 		if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
 		{
@@ -20,6 +20,10 @@ class MatrixMath
 			d = matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] - matrix[2, 1] * matrix[1, 2]) -
 				matrix[0, 1] * (matrix[0, 1] * matrix[2, 2] - matrix[2, 1] * matrix[0, 2]) +
 				matrix[0, 2] * (matrix[0, 1] * matrix[1, 2] - matrix[1, 1] * matrix[0, 2]);
+		}
+		else
+		{
+			return (-1);
 		}
 		return (d);
 	}
