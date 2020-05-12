c5e7948f4fd883e416558126f27df362f6595ba6
Marc <mcavigli@gmail.com>
2020-05-12 12:30:21 -0700

    Initial push

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+/// <summary>
+/// Base class
+/// </summary>
+public abstract class Base
+{
+	/// <summary>
+	/// Name given
+	/// </summary>
+	public string name { get; set; }
+
+	/// <summary>
+	/// Overrides default ToString() method.
+	/// </summary>
+	public override string ToString()
+	{
+		return name + " is a " + this.GetType();
+	}
+}
+
+interface IInteractive
+{
+	void Interact();
+}
+
+interface IBreakable
+{
+	int durability;
+	void Break();
+}
+
+interface ICollectable
+{
+	bool isCollected;
+	void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+	// code
+}
