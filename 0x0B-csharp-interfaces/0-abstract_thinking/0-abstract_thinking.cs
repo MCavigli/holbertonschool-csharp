f62f366b20eda548e3ebf5213200710187413486
Marc <mcavigli@gmail.com>
2020-05-12 12:09:45 -0700

    Added XML documentation

--- 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 
+/// <summary>
+/// Base class
+/// </summary>
 class Base
 {
 	public string name { get; set; }
 
+	/// <summary>
+	/// Overrides default ToString() method.
+	/// </summary>
 	public override string ToString()
 	{
 		return name + " is a " + typeof(SoftwareEngineer);
